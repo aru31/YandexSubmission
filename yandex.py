55060d33354a00676be270e5c5f0a3ba0247fd78
aru31 <guptarpit1997@gmail.com>
2019-02-11 04:11:24 +0530

    Bug Fix caused by numpy array used as pandas dataframe

--- yandex.py
+++ yandex.py
@@ -141,25 +141,43 @@ pca6 = PCA(n_components=1, random_state=0)
 pca7 = PCA(n_components=1, random_state=0)
 pca8 = PCA(n_components=1, random_state=0)
 
-new_array_train = np.empty(8)
-new_array_train[0] = pca1.fit_transform(closest_hits_features_train[0:4])
-new_array_train[1] = pca1.fit_transform(closest_hits_features_train[4:8])
-new_array_train[2] = pca1.fit_transform(closest_hits_features_train[8:12])
-new_array_train[3] = pca1.fit_transform(closest_hits_features_train[12:16])
-new_array_train[4] = pca1.fit_transform(closest_hits_features_train[16:20])
-new_array_train[5] = pca1.fit_transform(closest_hits_features_train[20:24])
-new_array_train[6] = pca1.fit_transform(closest_hits_features_train[24:28])
-new_array_train[7] = pca1.fit_transform(closest_hits_features_train[28:32])
 
-new_array_test = np.empty(8)
-new_array_test[0] = pca1.transform(closest_hits_features_test[0:4])
-new_array_test[1] = pca1.transform(closest_hits_features_test[4:8])
-new_array_test[2] = pca1.transform(closest_hits_features_test[8:12])
-new_array_test[3] = pca1.transform(closest_hits_features_test[12:16])
-new_array_test[4] = pca1.transform(closest_hits_features_test[16:20])
-new_array_test[5] = pca1.transform(closest_hits_features_test[20:24])
-new_array_test[6] = pca1.transform(closest_hits_features_test[24:28])
-new_array_test[7] = pca1.transform(closest_hits_features_test[28:32])
+a = pd.DataFrame(pca1.fit_transform(closest_hits_features_train.iloc[:, 0:4]))
+a.columns = [0]
+b = pd.DataFrame(pca2.fit_transform(closest_hits_features_train.iloc[:, 4:8]))
+b.columns = [1]
+c = pd.DataFrame(pca3.fit_transform(closest_hits_features_train.iloc[:, 8:12]))
+c.columns = [2]
+d = pd.DataFrame(pca4.fit_transform(closest_hits_features_train.iloc[:, 12:16]))
+d.columns = [3]
+e = pd.DataFrame(pca5.fit_transform(closest_hits_features_train.iloc[:, 16:20]))
+e.columns = [4]
+f = pd.DataFrame(pca6.fit_transform(closest_hits_features_train.iloc[:, 20:24]))
+f.columns = [5]
+g = pd.DataFrame(pca7.fit_transform(closest_hits_features_train.iloc[:, 24:28]))
+g.columns = [6]
+h = pd.DataFrame(pca8.fit_transform(closest_hits_features_train.iloc[:, 28:32]))
+h.columns = [7]
+new_array_train = pd.concat([a, b, c, d, e, f, g, h], axis=1)
+
+
+q = pd.DataFrame(pca1.transform(closest_hits_features_test.iloc[:, 0:4]))
+q.columns = [0]
+w = pd.DataFrame(pca2.transform(closest_hits_features_test.iloc[:, 4:8]))
+w.columns = [1]
+e = pd.DataFrame(pca3.transform(closest_hits_features_test.iloc[:, 8:12]))
+e.columns = [2]
+r = pd.DataFrame(pca4.transform(closest_hits_features_test.iloc[:, 12:16]))
+r.columns = [3]
+t = pd.DataFrame(pca5.transform(closest_hits_features_test.iloc[:, 16:20]))
+t.columns = [4]
+y = pd.DataFrame(pca6.transform(closest_hits_features_test.iloc[:, 20:24]))
+y.columns = [5]
+u = pd.DataFrame(pca7.transform(closest_hits_features_test.iloc[:, 24:28]))
+u.columns = [6]
+i = pd.DataFrame(pca8.transform(closest_hits_features_test.iloc[:, 28:32]))
+i.columns = [7]
+new_array_test = pd.concat([q, w, e, r, t, y, u, i], axis=1)
 
 # Okay so after this we will be adding new columns into the train dataset
 train['var1'] = new_array_train[0]
@@ -189,21 +207,33 @@ pca_new_4 = PCA(n_components=1, random_state=0)
 pca_new_5 = PCA(n_components=1, random_state=0)
 pca_new_6 = PCA(n_components=1, random_state=0)
 
-random_array_train = np.empty(6)
-random_array_train[0] = pca_new_1.fit_transform(train[['MatchedHit_X[0]', 'MatchedHit_X[1]', 'MatchedHit_X[2]', 'MatchedHit_X[3]']])
-random_array_train[1] = pca_new_2.fit_transform(train[['MatchedHit_Y[0]', 'MatchedHit_Y[1]', 'MatchedHit_Y[2]', 'MatchedHit_Y[3]']])
-random_array_train[2] = pca_new_3.fit_transform(train[['MatchedHit_Z[0]', 'MatchedHit_Z[1]']])
-random_array_train[3] = pca_new_4.fit_transform(train[['MatchedHit_DY[0]', 'MatchedHit_DY[1]']])
-random_array_train[4] = pca_new_5.fit_transform(train[['Lextra_X[0]', 'Lextra_X[1]', 'Lextra_X[2]', 'Lextra_X[3]']])
-random_array_train[5] = pca_new_6.fit_transform(train[['Lextra_Y[0]', 'Lextra_Y[1]', 'Lextra_Y[2]', 'Lextra_Y[3]']])
+ran1 = pd.DataFrame(pca_new_1.fit_transform(train[['MatchedHit_X[0]', 'MatchedHit_X[1]', 'MatchedHit_X[2]', 'MatchedHit_X[3]']]))
+ran1.columns = [0]
+ran2 = pd.DataFrame(pca_new_2.fit_transform(train[['MatchedHit_Y[0]', 'MatchedHit_Y[1]', 'MatchedHit_Y[2]', 'MatchedHit_Y[3]']]))
+ran2.columns = [1]
+ran3 = pd.DataFrame(pca_new_3.fit_transform(train[['MatchedHit_Z[0]', 'MatchedHit_Z[1]']]))
+ran3.columns = [2]
+ran4 = pd.DataFrame(pca_new_4.fit_transform(train[['MatchedHit_DY[0]', 'MatchedHit_DY[1]']]))
+ran4.columns = [3]
+ran5 = pd.DataFrame(pca_new_5.fit_transform(train[['Lextra_X[0]', 'Lextra_X[1]', 'Lextra_X[2]', 'Lextra_X[3]']]))
+ran5.columns = [4]
+ran6 = pd.DataFrame(pca_new_6.fit_transform(train[['Lextra_Y[0]', 'Lextra_Y[1]', 'Lextra_Y[2]', 'Lextra_Y[3]']]))
+ran6.columns = [5]
+random_array_train = pd.concat([ran1, ran2, ran3, ran4, ran5, ran6], axis=1)
 
-random_array_test = np.empty(6)
-random_array_test[0] = pca_new_1.transform(test[['MatchedHit_X[0]', 'MatchedHit_X[1]', 'MatchedHit_X[2]', 'MatchedHit_X[3]']])
-random_array_test[1] = pca_new_2.transform(test[['MatchedHit_Y[0]', 'MatchedHit_Y[1]', 'MatchedHit_Y[2]', 'MatchedHit_Y[3]']])
-random_array_test[2] = pca_new_3.transform(test[['MatchedHit_Z[0]', 'MatchedHit_Z[1]']])
-random_array_test[3] = pca_new_4.transform(test[['MatchedHit_DY[0]', 'MatchedHit_DY[1]']])
-random_array_test[4] = pca_new_5.transform(test[['Lextra_X[0]', 'Lextra_X[1]', 'Lextra_X[2]', 'Lextra_X[3]']])
-random_array_test[5] = pca_new_6.transform(test[['Lextra_Y[0]', 'Lextra_Y[1]', 'Lextra_Y[2]', 'Lextra_Y[3]']])
+rant1 = pd.DataFrame(pca_new_1.transform(test[['MatchedHit_X[0]', 'MatchedHit_X[1]', 'MatchedHit_X[2]', 'MatchedHit_X[3]']]))
+rant1.columns = [0]
+rant2 = pd.DataFrame(pca_new_2.transform(test[['MatchedHit_Y[0]', 'MatchedHit_Y[1]', 'MatchedHit_Y[2]', 'MatchedHit_Y[3]']]))
+rant2.columns = [1]
+rant3 = pd.DataFrame(pca_new_3.transform(test[['MatchedHit_Z[0]', 'MatchedHit_Z[1]']]))
+rant3.columns = [2]
+rant4 = pd.DataFrame(pca_new_4.transform(test[['MatchedHit_DY[0]', 'MatchedHit_DY[1]']]))
+rant4.columns = [3]
+rant5 = pd.DataFrame(pca_new_5.transform(test[['Lextra_X[0]', 'Lextra_X[1]', 'Lextra_X[2]', 'Lextra_X[3]']]))
+rant5.columns = [4]
+rant6 = pd.DataFrame(pca_new_6.transform(test[['Lextra_Y[0]', 'Lextra_Y[1]', 'Lextra_Y[2]', 'Lextra_Y[3]']]))
+rant6.columns = [5]
+random_array_test= pd.concat([rant1, rant2, rant3, rant4, rant5, rant6], axis=1)
 
 
 train['new_var1'] = random_array_train[0]
